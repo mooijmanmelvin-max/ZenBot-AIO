8072a6aa785d0bc622e2fd087e6aa4d73911dba6
Zenny <mooijman.melvin@gmail.com>
2026-06-12 02:21:25 +0200

    Change primary color in bot configuration

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#f6f1e7", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
